b56db8843b352a106bd2b0411ca0ed4c6a451579
phildinuzzo <phil248@gmail.com>
2014-12-31 12:25:32 -0800

    most animations complete

--- js.js
+++ js.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
 // Preload large images
   var preloads = ['assets/me.png', 'assets/goodfoodphone.png', 'assets/goodfoodscreen.png'];
@@ -8,238 +8,56 @@ $(document).ready(function(){
 		image.src = preloads[i];
 	}
 
-
-// Fade in the main div and check for hash
-  $('.header').delay(500).fadeIn(800, 'swing', function(){
-  	if(window.location.hash) {
-	  	$(window.location.hash).click();
-		}
-  });
-
-
-$.easing.jswing = $.easing.swing;
-
-$.extend($.easing,
-{
-    def: 'easeOutQuad',
-    swing: function (x, t, b, c, d) {
-        //alert($.easing.default);
-        return $.easing[$.easing.def](x, t, b, c, d);
-    },
-    easeInQuad: function (x, t, b, c, d) {
-        return c*(t/=d)*t + b;
-    },
-    easeOutQuad: function (x, t, b, c, d) {
-        return -c *(t/=d)*(t-2) + b;
-    },
-    easeInOutQuad: function (x, t, b, c, d) {
-        if ((t/=d/2) < 1) return c/2*t*t + b;
-        return -c/2 * ((--t)*(t-2) - 1) + b;
-    },
-    easeInCubic: function (x, t, b, c, d) {
-        return c*(t/=d)*t*t + b;
-    },
-    easeOutCubic: function (x, t, b, c, d) {
-        return c*((t=t/d-1)*t*t + 1) + b;
-    },
-    easeInOutCubic: function (x, t, b, c, d) {
-        if ((t/=d/2) < 1) return c/2*t*t*t + b;
-        return c/2*((t-=2)*t*t + 2) + b;
-    },
-    easeInQuart: function (x, t, b, c, d) {
-        return c*(t/=d)*t*t*t + b;
-    },
-    easeOutQuart: function (x, t, b, c, d) {
-        return -c * ((t=t/d-1)*t*t*t - 1) + b;
-    },
-    easeInOutQuart: function (x, t, b, c, d) {
-        if ((t/=d/2) < 1) return c/2*t*t*t*t + b;
-        return -c/2 * ((t-=2)*t*t*t - 2) + b;
-    },
-    easeInQuint: function (x, t, b, c, d) {
-        return c*(t/=d)*t*t*t*t + b;
-    },
-    easeOutQuint: function (x, t, b, c, d) {
-        return c*((t=t/d-1)*t*t*t*t + 1) + b;
-    },
-    easeInOutQuint: function (x, t, b, c, d) {
-        if ((t/=d/2) < 1) return c/2*t*t*t*t*t + b;
-        return c/2*((t-=2)*t*t*t*t + 2) + b;
-    },
-    easeInSine: function (x, t, b, c, d) {
-        return -c * Math.cos(t/d * (Math.PI/2)) + c + b;
-    },
-    easeOutSine: function (x, t, b, c, d) {
-        return c * Math.sin(t/d * (Math.PI/2)) + b;
-    },
-    easeInOutSine: function (x, t, b, c, d) {
-        return -c/2 * (Math.cos(Math.PI*t/d) - 1) + b;
-    },
-    easeInExpo: function (x, t, b, c, d) {
-        return (t==0) ? b : c * Math.pow(2, 10 * (t/d - 1)) + b;
-    },
-    easeOutExpo: function (x, t, b, c, d) {
-        return (t==d) ? b+c : c * (-Math.pow(2, -10 * t/d) + 1) + b;
-    },
-    easeInOutExpo: function (x, t, b, c, d) {
-        if (t==0) return b;
-        if (t==d) return b+c;
-        if ((t/=d/2) < 1) return c/2 * Math.pow(2, 10 * (t - 1)) + b;
-        return c/2 * (-Math.pow(2, -10 * --t) + 2) + b;
-    },
-    easeInCirc: function (x, t, b, c, d) {
-        return -c * (Math.sqrt(1 - (t/=d)*t) - 1) + b;
-    },
-    easeOutCirc: function (x, t, b, c, d) {
-        return c * Math.sqrt(1 - (t=t/d-1)*t) + b;
-    },
-    easeInOutCirc: function (x, t, b, c, d) {
-        if ((t/=d/2) < 1) return -c/2 * (Math.sqrt(1 - t*t) - 1) + b;
-        return c/2 * (Math.sqrt(1 - (t-=2)*t) + 1) + b;
-    },
-    easeInElastic: function (x, t, b, c, d) {
-        var s=1.70158;var p=0;var a=c;
-        if (t==0) return b;  if ((t/=d)==1) return b+c;  if (!p) p=d*.3;
-        if (a < Math.abs(c)) { a=c; var s=p/4; }
-        else var s = p/(2*Math.PI) * Math.asin (c/a);
-        return -(a*Math.pow(2,10*(t-=1)) * Math.sin( (t*d-s)*(2*Math.PI)/p )) + b;
-    },
-    easeOutElastic: function (x, t, b, c, d) {
-        var s=1.70158;var p=0;var a=c;
-        if (t==0) return b;  if ((t/=d)==1) return b+c;  if (!p) p=d*.3;
-        if (a < Math.abs(c)) { a=c; var s=p/4; }
-        else var s = p/(2*Math.PI) * Math.asin (c/a);
-        return a*Math.pow(2,-10*t) * Math.sin( (t*d-s)*(2*Math.PI)/p ) + c + b;
-    },
-    easeInOutElastic: function (x, t, b, c, d) {
-        var s=1.70158;var p=0;var a=c;
-        if (t==0) return b;  if ((t/=d/2)==2) return b+c;  if (!p) p=d*(.3*1.5);
-        if (a < Math.abs(c)) { a=c; var s=p/4; }
-        else var s = p/(2*Math.PI) * Math.asin (c/a);
-        if (t < 1) return -.5*(a*Math.pow(2,10*(t-=1)) * Math.sin( (t*d-s)*(2*Math.PI)/p )) + b;
-        return a*Math.pow(2,-10*(t-=1)) * Math.sin( (t*d-s)*(2*Math.PI)/p )*.5 + c + b;
-    },
-    easeInBack: function (x, t, b, c, d, s) {
-        if (s == undefined) s = 1.70158;
-        return c*(t/=d)*t*((s+1)*t - s) + b;
-    },
-    easeOutBack: function (x, t, b, c, d, s) {
-        if (s == undefined) s = 1.70158;
-        return c*((t=t/d-1)*t*((s+1)*t + s) + 1) + b;
-    },
-    easeInOutBack: function (x, t, b, c, d, s) {
-        if (s == undefined) s = 1.70158;
-        if ((t/=d/2) < 1) return c/2*(t*t*(((s*=(1.525))+1)*t - s)) + b;
-        return c/2*((t-=2)*t*(((s*=(1.525))+1)*t + s) + 2) + b;
-    },
-    easeInBounce: function (x, t, b, c, d) {
-        return c - $.easing.easeOutBounce (x, d-t, 0, c, d) + b;
-    },
-    easeOutBounce: function (x, t, b, c, d) {
-        if ((t/=d) < (1/2.75)) {
-            return c*(7.5625*t*t) + b;
-        } else if (t < (2/2.75)) {
-            return c*(7.5625*(t-=(1.5/2.75))*t + .75) + b;
-        } else if (t < (2.5/2.75)) {
-            return c*(7.5625*(t-=(2.25/2.75))*t + .9375) + b;
-        } else {
-            return c*(7.5625*(t-=(2.625/2.75))*t + .984375) + b;
-        }
-    },
-    easeInOutBounce: function (x, t, b, c, d) {
-        if (t < d/2) return $.easing.easeInBounce (x, t*2, 0, c, d) * .5 + b;
-        return $.easing.easeOutBounce (x, t*2-d, 0, c, d) * .5 + c*.5 + b;
-    }
-});
-
-// Some animation for fun
-	var navSlider = function(speed, i){
-			$('.slider-' + i).show().animate({
-				right: $('.navbar').width() - $('.active').position().left - ($('.active').width()/2) + 'px',
-
-			}, speed, function(){
-				$('.slider-' + i).fadeOut(500).css({
-					right: '-200px',
-				});
-
-				if (i === '0') {
-					$('.active').addClass('glow')
-					setTimeout(function(){
-						$('.active').removeClass('glow');
-					}, 1000)
-				}
-
-
-
+// Some navbar animation
+	var navSlider = function(speed, i) {
+		$('.slider-' + i).show().animate({
+			right: $('.navbar').width() - $('.active').position().left - ($('.active').width()/2) + 'px',
+		}, speed, function() {
+			$('.slider-' + i).fadeOut(500).css({
+				right: '-200px',
 			});
+
+			if (i === '0') {
+				$('.active').addClass('glow').one('animationiteration webkitAnimationIteration', function() {
+        	$(this).removeClass('glow');
+    		});
+			}
+		});
 	}
 
-// Navbar
-	$('.navbutton').on('click', function(e){
+// Change tabs function
+	var navAction = function(e, boolean) {
 		var self = this;
 		if (!$(this).hasClass('active')) {
+			// Navbar
 			$(this).addClass('active').siblings().removeClass('active');
+			// Maybe add more and iterate
+			if (!boolean === true) {
+				navSlider(450, '0');
+				navSlider(550, '1');
+				navSlider(650, '2');
+			}
 
-			navSlider(450, '0');
-			navSlider(550, '1');
-			navSlider(650, '2');
-
-
-
-			$('.content-container').children().fadeTo(500, 0.1);
-			$('.content-container').slideUp(400, function(){
-				$('#' + $(self).attr('id') + '-container').children().fadeTo(100, 1);
+			// Content
+			$('.content-container').fadeOut(400);
+			$('.container').animate({
+				height: $(this).data('height'),
+			}, 500, function() {
+				$('#' + $(self).attr('id') + '-container').fadeIn(300);
 			});
 
-			$('#' + $(this).attr('id') + '-container').delay(500).slideDown(500, function() {
-        $('#' + $(self).attr('id') + '-container').children().fadeTo(400, 1);
-      });
 		}
-	});
+	}
 
-
-$( "#clickme" ).click(function() {
-  $( "#book" ).animate({
-    opacity: 0.25,
-    left: "+=50",
-    height: "toggle"
-  }, 5000, function() {
-    // Animation complete.
+	// Fade in the main div and check for hash
+  $('.header').delay(500).fadeIn(800, 'swing', function() {
+  	if(window.location.hash) {
+  		$(window.location.hash).trigger('click', true);
+  	}
   });
-});
 
+  // Change tabs
+	$('.navbutton').on('click', navAction);
 
-
-
-
-
-
-
-
-
-
-
-// MODAL - OLD ~~~~~~~~~~~~~~~~~~~~~~~~~~
-  // $('.goodfoodModal').jqm({modal: true, trigger: '.modalTrigger'}).jqmAddClose(".close");
-
-  // $(window).on('resize', doResizeModal);
-  // $('.modalTrigger').on('click', doResizeModal);
-  //   function doResizeModal() {
-  //     $('.modal').css({
-  //         position:'absolute',
-  //         left: ($(window).width() - $('.modal').outerWidth())/2,
-  //         top: ($(window).height() - $('.modal').outerHeight())/2,
-  //         position:'fixed'
-
-  //     });
-  //   }
-
-  // $(window).scroll(function(e){
-  //   $el = $('.goodfoodModal');
-  //   if ($(this).scrollTop() > 200 && $el.css('position') != 'fixed'){
-  //     $('.goodfoodModal').css({'position': 'fixed', 'top': '0px'});
-  //   }
-  // });
-//  END MODAL ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 });
