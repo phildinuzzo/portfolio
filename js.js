8d5d5602c01f94372a2205c4ad8c7a896630a12c
phildinuzzo <phil248@gmail.com>
2015-01-09 15:08:49 -0800

    added content, fix to navbar

--- js.js
+++ js.js
@@ -1,5 +1,23 @@
 $(document).ready(function() {
 
+// NOTES
+
+// SPELLCHECK EVERYTHING
+// Add github link to portfolio items
+// Check navbar at small screen size (using hash links!)
+
+// CSS
+// Implement @font stuff for fonts
+// Organize media queries
+
+// Experience COPY
+// Experience RESIZE
+
+// Portfolio COPY
+// Portfolio LINKS (Git hub, project link)
+// Portfolio RESIZE
+
+
 // Preload large images, background first
   var preloads = ['assets/bg.png', 'assets/me.png', 'assets/goodfoodphone.png', 'assets/goodfoodscreen.png'];
 
@@ -28,10 +46,23 @@ $(document).ready(function() {
 		});
 	}
 
+  // Allow function to trigger only after resize is complete.
+  // var delay = (function(){
+  //   var timer = 0;
+  //   return function(callback, ms){
+  //     clearTimeout(timer);
+  //     timer = setTimeout(callback, ms);
+  //   };
+  // })();
+
 // Small screen navbar
 	$(window).on('resize', function(e){
 		if ($(e.target).width() < 700) {
-			$('.nav-arrow-container').show();
+      // delay(function(){
+        $('.nav-arrow-container').show();
+        // Temp fix for arrows on re-size to under 700px
+        $('.nav-arrow').css('top', $('.active').data('top-value'));
+      // }, 500);
 		} else {
 			$('.nav-arrow-container').hide();
 		}
@@ -51,12 +82,14 @@ $(document).ready(function() {
 				$('.nav-arrow-container').show();
 			}
 			// Small screen Navbar animation
-			var navHeight = $('.navbutton').outerHeight();
-			$('.nav-arrow').animate({
-				top: navHeight * $(this).data('top') - (navHeight/1.5) + 'px',
-			}, 500);
+      // if ($(window).width() < 700) {
+  			var navHeight = $('.navbutton').outerHeight();
+  			$('.nav-arrow').animate({
+  				top: navHeight * $(this).data('top') - (navHeight/1.5) + 'px',
+  			}, 500);
+      // }
 
-			// Content
+			// Content animation
 			$('.content-container').fadeOut(400);
 			$('.container').animate({
 				height: $(this).data('height'),
@@ -79,9 +112,16 @@ $(document).ready(function() {
 
   // Image modal
   var $overlay = $('.overlay');
-  $('.img-holder').on('click', function(){
+  var src;
+  $('.img-holder, .modal-image').on('click', function(){
   	$overlay.show();
-  	$('body').append('<div class="modal"><img src="' + $(this).children('img').attr('src') + '"></div>');
+  	if ($(this).attr('src')) {
+  		console.log('has clas')
+  		var src = $(this).attr('src')
+  	} else {
+  		var src = $(this).children('img').attr('src')
+  	}
+  	$('body').append('<div class="modal"><img src="' + src + '"></div>');
   	$('.modal').fadeIn(200);
   	$overlay.on('click', function(){
   		$overlay.fadeOut(300);
